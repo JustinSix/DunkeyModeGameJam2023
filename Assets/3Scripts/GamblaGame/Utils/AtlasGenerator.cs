afd26c8f2cb5c1f8180ce8b2d23082999a681135
JustinSix <thejustinsix@gmail.com>
2023-12-05 04:12:43 +0100

    gamble game working ish in build and in editor
    added if statement to atlas generator script so it only runs in editor,
    as it was preventing building of the game

---
+++ Assets/3Scripts/GamblaGame/Utils/AtlasGenerator.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using UnityEditor.U2D;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class AtlasGenerator {
+    [MenuItem("Tools/Create Sprite Atlas")]
+    public static void CreateSpriteAtlas() {
+        string[] spritePaths = AssetDatabase.GetAssetPathsFromAssetBundle("YourSpriteBundle");
+
+        SpriteAtlas atlas = new();
+        atlas.Add(StringToObject(spritePaths));
+        atlas.SetPackingSettings(new SpriteAtlasPackingSettings { });
+        atlas.SetTextureSettings(new SpriteAtlasTextureSettings { });
+        AssetDatabase.CreateAsset(atlas, "Assets/YourSpriteAtlas.spriteatlas");
+
+        AssetDatabase.Refresh();
+    }
+
+    private static Object[] StringToObject(string[] arr) {
+        int N = arr.Length;
+
+        Object[] arr2 = new Object[N];
+        for (int i = 0; i < N; i++) arr2[i] = AssetDatabase.LoadAssetAtPath<Object>(arr[i]);
+
+        return arr2;
+    }
+}
+#endif
